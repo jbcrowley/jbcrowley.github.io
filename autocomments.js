74fa385884bf3877c6c0360037c0efbad96c351b
jbcrowley <jbcrowley@hotmail.com>
2019-02-26 09:20:27 -0600

    Create autocomments.js

---
+++ autocomments.js
@@ -0,0 +1,38 @@
+callback(
+[
+{ "name": "[Q] Question needs the basics", "description": "Please read [ask], especially the part about [mcve] (MCVE), and <a href=\"https://meta.stackoverflow.com/questions/261592/how-much-research-effort-is-expected-of-stack-overflow-users\">How much research effort is expected?</a> This will help you debug your own programs and solve problems for yourself. If you do this and are still stuck you can come back and post your MCVE, what you tried, and the execution result including any error messages so we can better help you. Also provide a link to the page and/or the relevant HTML."},
+
+{ "name": "[Q] How do I do X?", "description": "See: <a href=\"https://meta.stackoverflow.com/questions/253069/whats-the-appropriate-new-current-close-reason-for-how-do-i-do-x\">How do I do X?</a> The expectation on SO is that the user asking a question not only does research to answer their own question but also shares that research, code attempts, and results. This demonstrates that you’ve taken the time to try to help yourself, it saves us from reiterating obvious answers, and most of all it helps you get a more specific and relevant answer! See also: [ask]"},
+
+{ "name": "[Q] Why isn't this code working?", "description": "Questions seeking debugging help (\"<strong>why isn't this code working?</strong>\") must include the desired behavior, a <em>specific problem or error and the shortest code necessary</em> to reproduce it <strong>in the question itself</strong>. Questions without a <strong>clear problem statement</strong> are not useful to other readers. See: [mcve]."},
+
+{ "name": "[Q] Why is “Can someone help me?” not an actual question?", "description": "Please read <a href=\"https://meta.stackoverflow.com/questions/284236/why-is-can-someone-help-me-not-an-actual-question\">Why is “Can someone help me?” not an actual question?</a>. Then read [ask] and take a few minutes to add some details to your question, especially an [mcve]."},
+
+{ "name": "[Q] Recommendations", "description": "Questions asking us to recommend or find a book, tool, software library, tutorial or other off-site resource are off-topic for $SITENAME$ as they tend to attract opinionated answers and spam. Instead, <a href=\"https://meta.stackoverflow.com/questions/254393/what-exactly-is-a-recommendation-question\">describe the problem</a> and what has been done so far to solve it."},
+
+{ "name": "[Q] Unclear what you are asking", "description": "Please clarify your specific problem or add additional details to highlight exactly what you need. As it's currently written, it’s hard to tell exactly what you're asking. See the [ask] page for help clarifying this question."},
+
+{ "name": "[Q] Screenshot of code", "description": "Sorry, we [can't accept images of code or errors](https://meta.stackoverflow.com/a/285557). Post those as text, so that we can try to reproduce the problem without having to re-type everything, and your question can be properly indexed or read by screen readers."},
+
+{ "name": "[Q] Too broad", "description": "Please edit the question to limit it to a specific problem with enough detail to identify an adequate answer. Avoid asking multiple distinct questions at once. See the [ask] page for help clarifying this question."},
+
+{ "name": "[Q] Primarily opinion-based", "description": "Many good questions generate some degree of opinion based on expert experience, but answers to this question will tend to be almost entirely based on opinions, rather than facts, references, or specific expertise."},
+
+{ "name": "[Q] Format HTML and/or code", "description": "When you post HTML and/or code please take a minute to use a beautifier like http://jsbeautifier.org/ or your IDE to properly format everything. If you need help properly formatting it on the site, see the formatting help link in the sidebar of the question editor. It makes it a LOT easier to read which makes your question more likely to get answered. Thanks!"},
+
+{ "name": "[Q] More than one question asked", "description": "It is preferred if you can post separate questions instead of combining your questions into one. That way, it helps the people answering your question and also others hunting for at least one of your questions. Thanks!"},
+
+{ "name": "[Q] Duplicate closure", "description": "This question will probably be closed as a duplicate soon. If the answers from the duplicates don't fully address your question please edit it to include why and flag this for re-opening. Thanks!"},
+
+{ "name": "[A] Upvote and accept an answer", "description": "If this, or any other answer, was useful please upvote it. Once you find the answer to your question, please mark it as accepted so the question isn't left unanswered."},
+
+{ "name": "[A] URL only answer", "description": "While this may theoretically answer the question, it would be preferable to include the essential parts of the answer here, and provide the link for reference."},
+
+{ "name": "[A] Requests to OP for further information", "description": "This is really a comment, not an answer. With a bit more rep, you will be able to post comments."},
+
+{ "name": "[A] OP using an answer for further information", "description": "Please use the Post answer button only for actual answers. You should modify your original question to add additional information."},
+
+{ "name": "[A] OP adding a new question as an answer", "description": "If you have another question, please ask it by clicking the Ask Question button after thoroughly searching for the answer."},
+
+]
+)
